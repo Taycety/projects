1c8aafc8e541f16fb38a7bc281af17f4ec0ad8b3
Taycety <1435401438@qq.com>
2022-09-29 09:48:08 +0800

    项目基本配置和跨域

--- ty/vue.config.js
+++ ty/vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 项目打包白屏
+  publicPath: "./",
+  devServer: {
+    // 不开启https
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeorigin: true,
+        pathRewirte: {
+          ["^" + process.env.VUE_APP_BASE_API]: ""
+        }
+      }
+    }
+  }
 })
